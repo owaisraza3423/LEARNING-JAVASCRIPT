36f3595c029e51205e85fa181eece5e76f82708f
owaisraza <owaisrazajanmohammad@gmail.com>
2025-01-23 21:31:38 +0500

    Initial commit

---
+++ JAVASCRIPT DAY 2/main.js
@@ -0,0 +1,16 @@
+// console.log("hello,world", 4+6,"another log")
+// console.assert(4+5);
+// console.log("hello,world");
+// document.write("this is a rough document");
+// alert("me");
+// console.warn("this is last warning");
+// console.error("this is error");
+
+
+// LEARNING ABOUT THE VARIABLES
+var number1 = 56;
+var number2 = 44;
+
+console.log(number1 + number2);
+
+
---
+++ JAVASCRIPT DAY 3 PRIMILATIVE OBJECTS/main.js
@@ -0,0 +1,47 @@
+// nn ss bb u
+// number
+var number1 = 244;
+var number2 = 345;
+console.log(number1 + number2);
+console.log(typeof number1);
+
+
+// string
+var str1 = "this is a string";
+var str2 = "this is also a string";
+console.log(str1 , str2);
+console.log(typeof str1 );
+
+// object
+var marks = {
+    owais:245,
+    hassan:343,
+    arham:354,
+}
+console.log(marks)
+console.log(typeof Object)
+
+
+
+// boolean 
+
+
+var a = true;
+var b = false;
+console.log(a)
+ console.log(typeof a ,typeof b )
+
+
+
+// undefind 
+var und = undefined ;
+var und ;
+console.log(und)
+
+
+// null 
+var n = null ;
+console.log(n);
+console.log(typeof n);
+
+
