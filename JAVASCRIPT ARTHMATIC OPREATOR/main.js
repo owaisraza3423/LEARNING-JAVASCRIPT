8cabfb935bcf5142325ab3b6f75881a457e25471
owaisraza <owaisrazajanmohammad@gmail.com>
2025-01-25 20:57:17 +0500

    arthmatic and logical operators

---
+++ JAVASCRIPT ARTHMATIC OPREATOR/main.js
@@ -0,0 +1,21 @@
+// arthmatic operator
+
+var a = 100;
+var b = 10;
+
+console.log("the value of a+b is ", a + b );
+console.log("the value of a+b is ", a - b );
+console.log("the value of a+b is ", a * b );
+console.log("the value of a+b is ", a / b );
+
+// Assignment operators
+var c = a
+c+=43;
+// all way using for
+console.log(c)
+var x =34;
+var y =21;
+
+console.log(x == y);
+console.log(x <= y);
+console.log(x >= y);
