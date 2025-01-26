f25140514c2164cca951e314ef062ae489eecfc0
owaisraza <owaisrazajanmohammad@gmail.com>
2025-01-26 11:23:39 +0500

    function:learing functions

---
+++ JAVASCRIPT FUNCTION/main.js
@@ -0,0 +1,9 @@
+// console.log("hello Functions";
+function avg (a ,b){ 
+     c1 = (a + b/2);
+     return c2;
+}
+
+c = avg (4,6);
+c2 =avg (12,16);
+console.log(c , c2);
---
+++ JAVASCRIPT LOGICAL OPERATORS /main.js
@@ -0,0 +1,13 @@
+console.log(true&&true);
+console.log(true&&false);
+console.log(false&&false);
+console.log(false&&true);
+
+
+console.log(!true);
+console.log(!false);
+
+
+function avg(a,b){
+    return(a + b)/2;
+}
