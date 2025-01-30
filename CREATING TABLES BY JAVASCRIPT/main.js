69695867481a9d5e7be84036232920326c6ab68b
owaisraza <owaisrazajanmohammad@gmail.com>
2025-01-30 19:55:20 +0500

    TABLE : CREATING TABLE BY JAVASCRIPT

---
+++ CREATING TABLES BY JAVASCRIPT/main.js
@@ -0,0 +1,12 @@
+let tableCounter = + prompt("select your number");
+let table = 1;
+console.log(tableCounter + "x" + table + "=" + table++ *tableCounter);
+console.log(tableCounter + "x" + table + "=" + table++ *tableCounter);
+console.log(tableCounter + "x" + table + "=" + table++ *tableCounter);
+console.log(tableCounter + "x" + table + "=" + table++ *tableCounter);
+console.log(tableCounter + "x" + table + "=" + table++ *tableCounter);
+console.log(tableCounter + "x" + table + "=" + table++ *tableCounter);
+console.log(tableCounter + "x" + table + "=" + table++ *tableCounter);
+console.log(tableCounter + "x" + table + "=" + table++ *tableCounter);
+console.log(tableCounter + "x" + table + "=" + table++ *tableCounter);
+console.log(tableCounter + "x" + table + "=" + table++ *tableCounter);
